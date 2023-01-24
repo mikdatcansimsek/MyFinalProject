a94f5bd4c3b83e495122ed0efb7e94d88bcab73a
Mitat Can Şimşek <mit_can006@hotmail.com>
2023-01-24 23:36:48 +0300

    9.Hafta Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -6,25 +6,26 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //ProductTest();
+        ProductTest();
+        //CategoryTest();
+    }
 
-        CategoryManager categoryManager=new CategoryManager(new EfCategoryDal());
-
+    private static void CategoryTest()
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
         foreach (var category in categoryManager.GetAll())
         {
             Console.WriteLine(category.CategoryName);
         }
-
-
     }
 
     private static void ProductTest()
     {
         ProductManager productManager = new ProductManager(new EfProductDal());
 
-        foreach (var product in productManager.GetByUnitPrice(50, 100))
+        foreach (var product in productManager.GetProductDetails())
         {
-            Console.WriteLine(product.ProductName);
+            Console.WriteLine(product.ProductName + "/" + product.CategoryName);
         }
     }
 }
