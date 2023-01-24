55bd25ee301846e0530f4cd1a6a849b5b0d0570f
Mitat Can Şimşek <mit_can006@hotmail.com>
2023-01-24 23:08:25 +0300

    24 Ocak

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -6,13 +6,25 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        //ProductTest();
+
+        CategoryManager categoryManager=new CategoryManager(new EfCategoryDal());
+
+        foreach (var category in categoryManager.GetAll())
+        {
+            Console.WriteLine(category.CategoryName);
+        }
+
+
+    }
+
+    private static void ProductTest()
+    {
         ProductManager productManager = new ProductManager(new EfProductDal());
 
-        foreach (var product in productManager.GetByUnitPrice(50,100))
+        foreach (var product in productManager.GetByUnitPrice(50, 100))
         {
             Console.WriteLine(product.ProductName);
         }
-
-        
     }
 }
--- DataAccess/Abstract/ICategoryDal.cs
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.DataAccess;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,5 @@ namespace DataAccess.Abstract
 {
     public interface ICategoryDal:IEntityRepository<Category>
     {
-        
     }
 }
